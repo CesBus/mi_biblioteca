9e0609e7e86c500007762fb012abb7f591584bd9
Ludwig Valdenegro <ludwigval@gmail.com>
2022-07-07 18:10:15 -0400

    Agrega cambios antes de subir a github

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'books/index'
+  get 'books/new'
+  post 'books', to: 'books#create'
+
+  put 'books/:id', to: 'books#update'
+  patch 'books/:id', to: 'books#update'
+
+  # /books/1/edit
+  get 'books/:id/edit', to: 'books#edit', as: 'books_edit'
+  # /books/1
+  get 'books/:id', to: 'books#show', as: 'book'
+
+  root 'books#index'
 end
